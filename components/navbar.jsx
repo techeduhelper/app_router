d1fc46e9eba729195db4acbc2a22f97f5db01931
techeduhelper <uccmaniruddinkhan@gmail.com>
2023-09-05 14:06:47 +0530

    my system not smoothly run

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -2,8 +2,13 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="fixed flex w-full mt-10 right-0 left-0 bg-slate-300 shadow-md ">
-      Food Recipe
+    <div className="fixed flex w-full mt-10 right-0 left-0 bg-slate-300 shadow-md h-10 my-2 px-4">
+      <div>
+
+      </div>
+      <div>
+        
+      </div>
     </div>
   );
 };
